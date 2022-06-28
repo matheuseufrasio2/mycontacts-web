16926bc982ef0befbd337e67d5f704f4e91a1656
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-06-28 06:28:48 -0300

    create header

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import {
+  Container,
+  InputSearchContainer,
+} from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="MyContacts" width="201" />
+
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+    </Container>
+  );
+}
