32abaea0873e3353d2e359ccaa8969b5dc90def5
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-07-05 05:31:38 -0300

    create input error message

--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -4,14 +4,20 @@ import {
   Container,
 } from './styles';
 
-export function FormGroup({ children }) {
+export function FormGroup({ children, error }) {
   return (
     <Container>
       {children}
+      {error && <small>{error}</small>}
     </Container>
   );
 }
 
 FormGroup.propTypes = {
   children: PropTypes.node.isRequired,
+  error: PropTypes.string,
+};
+
+FormGroup.defaultProps = {
+  error: null,
 };
