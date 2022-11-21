bb62fa34bd53274aea17ed47b83d391af1a9fd86
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-11-21 11:02:15 -0300

    animate Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -5,15 +5,18 @@ import {
 } from './styles';
 import { Spinner } from '../Spinner';
 import { ReactPortal } from '../ReactPortal';
+import { useAnimatedUnmount } from '../../hooks/useAnimatedUnmount';
 
 export function Loader({ isLoading }) {
-  if (!isLoading) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
